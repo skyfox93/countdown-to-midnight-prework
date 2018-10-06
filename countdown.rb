49bb95604f1e87ef0c4dd8e23f716cbbe5e5dff9
Skylar Salerno <skylar.salerno@gmail.com>
2018-10-06 17:54:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,17 @@ def countdown(num)
   count=num;
   while num>0
   "#{number} SECONDS"
+    num-=1
+  end
+  "HAPPY NEW YEAR"
+end
+
+def countdown_with_sleep(num)
+  count=num;
+  while num>0
+  "#{number} SECONDS"
+  sleep(num)
+  num-=1;
   end
   "HAPPY NEW YEAR"
 end
